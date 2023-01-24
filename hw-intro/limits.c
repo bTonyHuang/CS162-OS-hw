aadb66cabe2130485b20ebed2589febb109d8250
Zixun Huang <tonyhuang@berkeley.edu>
2023-01-24 19:50:52 +0000

    finish limits.c

--- hw-intro/limits.c
+++ hw-intro/limits.c
@@ -3,8 +3,33 @@
 
 int main() {
     struct rlimit lim;
-    printf("stack size: %ld\n", 0L);
-    printf("process limit: %ld\n", 0L);
-    printf("max file descriptors: %ld\n", 0L);
+    long stackSize=0;
+    long proLimit=0;
+    long maxFile=0;
+
+    if(!getrlimit(RLIMIT_STACK,&lim))
+        stackSize=lim.rlim_cur;
+    else{
+        printf("function failed\n");
+        return 1;
+    }
+
+    if(!getrlimit(RLIMIT_NPROC,&lim))
+        proLimit=lim.rlim_cur;
+    else{
+        printf("function failed\n");
+        return 1;
+    }
+
+    if(!getrlimit(RLIMIT_NOFILE,&lim))
+        maxFile=lim.rlim_cur;
+    else{
+        printf("function failed\n");
+        return 1;
+    }
+
+    printf("stack size: %ld\n", stackSize);
+    printf("process limit: %ld\n", proLimit);
+    printf("max file descriptors: %ld\n", maxFile);
     return 0;
 }
