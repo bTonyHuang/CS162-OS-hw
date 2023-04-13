53edf7b42ee09fad6c0d8f3f062c7dba0d76ca33
Zixun Huang <tonyhuang@berkeley.edu>
2023-04-13 21:54:38 +0000

    finish get put rpc, use RwLock to protect HashMap

--- lab-grpc-rs/src/client.rs
+++ lab-grpc-rs/src/client.rs
@@ -10,6 +10,7 @@ async fn connect() -> Result<kv_store_client::KvStoreClient<Channel>> {
 }
 
 // Client methods. DO NOT MODIFY THEIR SIGNATURES.
+/*server would return input+1*/
 pub async fn example(input: u32) -> Result<u32> {
     let mut client = connect().await?;
     Ok(client
@@ -19,16 +20,27 @@ pub async fn example(input: u32) -> Result<u32> {
         .output)
 }
 pub async fn echo(msg: String) -> Result<String> {
-    let mut client=connect().await?;
+    let mut client = connect().await?;
     Ok(client
         .echo(EchoRequest{msg})
         .await?
         .into_inner()
         .msg)
 }
+/*Put should take in a key and value (both of type bytes) and store the pair in a hash map. 
+If the key is already in the hash map, the previous value should be overwritten.*/
 pub async fn put(key: Vec<u8>, value: Vec<u8>) -> Result<()> {
-    todo!("TODO: KV store")
+    let mut client = connect().await?;
+    client.put(PutRequest{key,value}).await?;
+    Ok(())
 }
+ /*Get should take in a key of type bytes and return the result of looking up the value in the hash map. 
+    If the key is not found, return Err(tonic::Status::new(tonic::Code::NotFound, "Key does not exist.")).*/
 pub async fn get(key: Vec<u8>) -> Result<Vec<u8>> {
-    todo!("TODO: KV store")
+    let mut client = connect().await?;
+    Ok(client
+        .get(GetRequest{key})
+        .await?
+        .into_inner()
+        .value)
 }
--- lab-grpc-rs/src/server.rs
+++ lab-grpc-rs/src/server.rs
@@ -5,7 +5,13 @@ use crate::{log, rpc::kv_store::*, SERVER_ADDR};
 use anyhow::Result;
 use tonic::{transport::Server, Request, Response, Status};
 
-pub struct KvStore {}
+use std::collections::HashMap;
+use tokio::sync::RwLock;
+use std::sync::Arc;
+
+pub struct KvStore {
+    kv_map_ptr: Arc<RwLock<HashMap<Vec<u8>,Vec<u8>>>>,
+}
 
 #[tonic::async_trait]
 impl kv_store_server::KvStore for KvStore {
@@ -19,7 +25,6 @@ impl kv_store_server::KvStore for KvStore {
         }))
     }
 
-    // TODO: RPC implementation
     async fn echo(
         &self,
         req: Request<EchoRequest>,
@@ -29,10 +34,45 @@ impl kv_store_server::KvStore for KvStore {
             msg: req.into_inner().msg,
         }))
     }
+
+    /*Put should take in a key and value (both of type bytes) and store the pair in a hash map. 
+    If the key is already in the hash map, the previous value should be overwritten.*/
+    async fn put(
+        &self,
+        req: Request<PutRequest>,
+    ) -> Result<Response<PutReply>, Status> {
+        log::info!("Received put request.");
+        let req2 = req.into_inner().clone();
+        let key = req2.key;
+        let value = req2.value;
+        /*check if key in map, if not, insert*/
+        let kv_map = &mut self.kv_map_ptr.write().await;
+        kv_map.entry(key).and_modify(|usize| *usize = value.clone()).or_insert(value);
+
+        Ok(Response::new(PutReply{}))
+    }
+
+    /*Get should take in a key of type bytes and return the result of looking up the value in the hash map. 
+    If the key is not found, return Err(tonic::Status::new(tonic::Code::NotFound, "Key does not exist.")).*/
+    async fn get(
+        &self,
+        req: Request<GetRequest>,
+    ) -> Result<Response<GetReply>, Status> {
+        log::info!("Received get request.");
+        let key = req.into_inner().key;
+        /*check if key in map*/
+        let kv_map = &self.kv_map_ptr.read().await;
+        match kv_map.get(&key){
+            Some(val) => Ok(Response::new(GetReply{value: val.to_vec()})),
+            None => Err(tonic::Status::new(tonic::Code::NotFound, "Key does not exist.")),
+        }
+    }
 }
 
 pub async fn start() -> Result<()> {
-    let svc = kv_store_server::KvStoreServer::new(KvStore {});
+    let mut kv_map: HashMap<Vec<u8>,Vec<u8>> = HashMap::new();
+    let mut kv_map_ptr = Arc::new(RwLock::new(kv_map));
+    let svc = kv_store_server::KvStoreServer::new(KvStore {kv_map_ptr});
 
     log::info!("Starting KV store server.");
     Server::builder()
